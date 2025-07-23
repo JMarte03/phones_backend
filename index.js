4d163732e315ea8e4dd1aa54462a90874ab055dc
JMarte03 <jesimielmarte.dev@gmail.com>
2025-07-23 15:48:40 -0400

    build ui

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
-app.use(express.json())
-app.use(cors())
 
+app.use(express.json())
+app.use(express.static('dist'))
+app.use(cors())
 
 morgan.token('body', (req, res) =>  { 
     return JSON.stringify(req.body) 
